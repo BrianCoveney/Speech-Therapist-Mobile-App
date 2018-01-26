5ad448d011cbaaf655b6d0cac6f0efb9127c7186
BrianCoveney <coveneybrian@gmail.com>
2018-01-26 11:59:48 +0000

    Add login screen

--- app/src/main/java/com/brian/speechtherapistapp/MainActivity.java
+++ app/src/main/java/com/brian/speechtherapistapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.brian.speechtherapistapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView bubblesImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
